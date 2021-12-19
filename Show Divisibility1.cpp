967e7438d066fc2d7df3e1a869449c855afc831c
Omid Tahmasbi <91671463+OmidTahmasbi1994@users.noreply.github.com>
2021-12-20 02:04:49 +0330

    Add files via upload

---
+++ Show Divisibility1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+
+{
+	int a , b;
+	cout<<"Please Enter two positive Number:"<<endl;
+	cout<<"Enter Number a:"<<endl;
+	cin>>a;
+	cout<<"Enter Number b:"<<endl;
+	cin>>b;
+	
+	if (a%b) cout<<a<<"  is not divisible by  "<<b<<endl;
+	else cout<<a<<"  is divisible  by  "<<b<<endl;
+	
+	getch();
+}
